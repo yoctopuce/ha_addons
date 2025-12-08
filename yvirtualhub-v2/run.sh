79067d0f098bb6261c021a0f1420b7144b71a5dd
Yoctopuce <dev@yoctopuce.com>
2025-12-08 19:08:04 +0100

    Test fix

--- yvirtualhub-v2/run.sh
+++ yvirtualhub-v2/run.sh
@@ -8,6 +8,8 @@ else
   VHUB_BIN="./${BUILD_ARCH}/VirtualHub-V2"
 fi
 
+echo "Wait 10 secondes.."
+sleep 10
 echo "Start VirtualHub-V2 (${VHUB_BIN})"
 ${VHUB_BIN} -c /data/.virtualhubv2/.virtualhub.dat -k /data/.virtualhubv2/.virtualhub.key -s /data/.virtualhubv2/.virtualhub.cert 2>&1
 echo "End of VirtualHub-V2 for ${BUILD_ARCH}"
