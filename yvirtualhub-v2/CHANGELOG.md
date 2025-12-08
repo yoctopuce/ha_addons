dd12232fbeb3b64c922d5aaec3bca6e0b484c387
Yoctopuce <dev@yoctopuce.com>
2025-12-08 18:41:17 +0100

    Release 1.10.70607

--- yvirtualhub-v2/CHANGELOG.md
+++ yvirtualhub-v2/CHANGELOG.md
@@ -1,3 +1,16 @@
+## 2.1.10607
+
+- Add --renew_cert option
+- Prevent set of admin or user password containing @ and / character
+- Fix problems with Yocto-3D interface
+- Fix service installation on Linux
+- Fix error when trying to get information about firmware for a non-standard device
+- Fix SSDP discovery with IPv4
+- Fix handling of command line options during service installation
+- Fix parsing of deeply nested JSON structures
+- Fix for CRC in UI file
+
+
 ## 2.1.9141
 
 - First release
--- yvirtualhub-v2/run.sh
+++ yvirtualhub-v2/run.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/with-contenv bashio
 lsusb
-echo "start VirtualHub-V2"
+mkdir -p /data/.virtualhubv2
+# start binary in background
+if [ "${BUILD_ARCH}" = "amd64" ]; then
+  VHUB_BIN="./x86_64/VirtualHub-V2"
+else
+  VHUB_BIN="./${BUILD_ARCH}/VirtualHub-V2"
+fi
+
+echo "Start VirtualHub-V2 (${VHUB_BIN})"
+${VHUB_BIN} -c /data/.virtualhubv2/.virtualhub.dat -k /data/.virtualhubv2/.virtualhub.key -s /data/.virtualhubv2/.virtualhub.cert 2>&1
+echo "End of VirtualHub-V2 for ${BUILD_ARCH}"
+
+tail -f /dev/null
